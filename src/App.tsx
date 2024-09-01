0384e22530d1ced00949f808e25075428f822703
Wendel_dev <tybnkdbj35@gmail.com>
2024-09-01 14:36:51 -0300

    feat: add import of components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-
+import Banner from "./components/landingpageComponents/Banner";
+import Container from "./components/landingpageComponents/Container";
+import CTA from "./components/landingpageComponents/CTA";
+import Footer from "./components/landingpageComponents/Footer";
+import Header from "./components/landingpageComponents/Header";
+import MembersClubBanner from "./components/landingpageComponents/MembersClubBanner";
+import Plans from "./components/landingpageComponents/Plans";
+import AtomicBanner from "./components/landingpageComponents/AtomicBanner";
+import CommonQuestions from "./components/landingpageComponents/CommonQuestions";
+import TopButton from "./components/landingpageComponents/TopButton";
 
 const App: React.FC = () => {
 	return (
-		<div>
-			Hello World
-		</div>
+		<Container>
+			<Header />
+			<Banner />
+			<AtomicBanner />
+			<CTA />
+			<MembersClubBanner />
+			<Plans />
+			<CommonQuestions />
+			<Footer />
+			<TopButton />
+		</Container>
 	);
 };
 
